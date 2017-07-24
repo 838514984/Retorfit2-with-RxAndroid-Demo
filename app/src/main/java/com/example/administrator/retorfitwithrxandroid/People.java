d4ec911925667a3d5ceabed6f3c9019549a0a969
secretbase <838514984@qq.com>
2017-07-24 11:51:25 +0800

    genius

---
+++ app/src/main/java/com/example/administrator/retorfitwithrxandroid/People.java
@@ -0,0 +1,17 @@
+package com.example.administrator.retorfitwithrxandroid;
+
+/**
+ * Created by Administrator on 2017/7/21 0021.
+ */
+
+public class People {
+    public People(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    public int age;
+    public String name;
+    public String sex;
+
+}
